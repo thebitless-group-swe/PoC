6c6bd23a2a7a78c054c7e5dc81765fc8d77d05b8
marinamarini04 <marini.anna12@gmail.com>
2026-06-13 17:37:20 +0200

    chore(api): aggiungi stub backend scaffolding V4

--- api/app/routes/__init__.py
+++ api/app/routes/__init__.py
@@ -1,3 +1,5 @@
 from .summarize import router as summarize_router
+from .generate import router as generate_router
+from .generate_link import router as generate_link_router
 
-__all__ = ["summarize_router"]
+__all__ = ["summarize_router", "generate_router", "generate_link_router"]
